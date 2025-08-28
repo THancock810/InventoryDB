35463f04d85f495879dc02ec46f94b253c3ac898
Tom <Tom@DESKTOP-4MBSLIG>
2025-08-28 15:20:25 -0600

    Add project files.

---
+++ Landing.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace InventoryDB
+{
+    partial class Landing
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(575, 210);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(437, 367);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // Landing
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1617, 976);
+            Controls.Add(groupBox1);
+            Name = "Landing";
+            Text = "Inventory";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+    }
+}
---
+++ Landing.cs
@@ -0,0 +1,17 @@
+using System.Data.SQLite;
+
+namespace InventoryDB
+{
+    public partial class Landing : Form
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
